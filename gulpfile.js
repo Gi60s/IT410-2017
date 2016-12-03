7b6f57ec2acbb9c559b6d3071f78b5949bc3ae26
James Speirs <james.speirs@gmail.com>
2016-12-03 14:22:02 -0700

    define build

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+const gulp          = require('gulp');
+const marked        = require('gulp-marked');
+const pygmentize    = require('pygmentize-bundled');
+
+gulp.task('convert', function() {
+    gulp.src('./src/*.md')
+        .pipe(marked({
+            highlight: function (code, lang, callback) {
+                pygmentize({ lang: lang, format: 'html' }, code, function (err, result) {
+                    callback(err, result.toString());
+                });
+            }
+        }))
+        .pipe(gulp.dest('./lessons/'))
+});
+
