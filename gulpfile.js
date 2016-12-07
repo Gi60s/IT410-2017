625aeb6489fe701ee7558b967ba3cc107d9bdd4c
James Speirs <james.speirs@gmail.com>
2016-12-06 20:50:17 -0700

    add watch

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,17 @@ const pygmentize    = require('pygmentize-bundled');
 gulp.task('convert', function() {
     gulp.src('./src/*.md')
         .pipe(marked({
-            highlight: function (code, lang, callback) {
+            /*highlight: function (code, lang, callback) {
                 pygmentize({ lang: lang, format: 'html' }, code, function (err, result) {
-                    callback(err, result.toString());
+                    callback(err, result);
                 });
-            }
+            }*/
         }))
         .pipe(gulp.dest('./www/lessons/'))
 });
 
+gulp.task('watch', function() {
+    gulp.watch('./src/*.md', ['convert']);
+});
+
+gulp.task('default', ['watch']);
