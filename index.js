100b515550b12e22dcb46821200cd645332a1d78
James Speirs <james.speirs@gmail.com>
2017-01-07 15:23:45 -0700

    add api endpoints

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const path              = require('path');
 
 const app = express();
 const www = path.resolve(__dirname, 'www');
+const start = Date.now();
+
+app.get('/api/status', function(req, res) {
+    res.send('Uptime: ' + Math.round((Date.now() - start) / 1000) + ' seconds');
+});
+
+app.get('/api/grade/:assignmentId/:ghUser/:ghRepository', function(req, res) {
+    res.send('ok');
+});
 
 app.get('*', function(req, res) {
     const url = req.url.split('?')[0];
@@ -21,7 +30,7 @@ app.get('*', function(req, res) {
     }
 });
 
-app.listen(8080, function(err) {
+app.listen(80, function(err) {
     if (err) return console.error(err.stack);
-    console.log('Listening on port: 8080');
+    console.log('Listening on port: 80');
 });
