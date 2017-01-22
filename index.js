12c290e7e481fc1930d2fd1d0f84cf3ed009c47b
James Speirs <james.speirs@gmail.com>
2017-01-21 21:36:15 -0700

    console log return lines

--- index.js
+++ index.js
@@ -54,7 +54,9 @@ app.get('/api/grade/:assignmentId/:ghUser/:ghRepository/:date?', function(req, r
                 result += '<h2>Details</h2>';
                 result += '<pre>' + lines.slice(0, lines.length - 2).join('\n') + '</pre>';
 
-                result += '<script>console.log("' + raw.replace(/"/g, '\\"').replace(/\r\n/g, '\\\\n') + '");</script>';
+                const codes = raw.split('').map(v => v.charCodeAt(0));
+                result += '<script>(function() { var data = [' + codes.join(',') +
+                    '].map(function(v) { return String.fromCharCode(v); }).join(""); console.log(data); })();</script>';
             } else {
                 result += '<h2>Something Went Wrong</h2>';
                 result += '<pre>' + raw + '</pre>';
